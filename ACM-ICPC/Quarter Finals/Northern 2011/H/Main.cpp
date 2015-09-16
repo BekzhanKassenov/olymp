9c62e0f942a1326a27a72dcc43bc646f8e644e76
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2015-09-16 17:38:40 +0600

    Solved problem H from Northern 2011

--- ACM-ICPC/Quarter Finals/Northern 2011/H/Main.cpp
+++ ACM-ICPC/Quarter Finals/Northern 2011/H/Main.cpp
@@ -34,8 +34,9 @@ string s[MAXN];
 map <long long, int> cnt[1 << 5];
 long long ans[6];
 long long str[MAXN][1 << 5];
+int C[10][10];
 
-int code(char c) {
+inline int code(char c) {
     if (isdigit(c)) {
         return c - '0';
     }
@@ -47,21 +48,13 @@ int code(char c) {
     return 36 + c - 'A';
 }
 
-long long _hash(const string& s) {
+inline long long _hash(const string& s, int mask) {
     long long res = 0;
-    for (char c: s) {
-        res *= BASE;
-        res += code(c);
-    }
 
-    return res;
-}
-
-string get(const string& s, int mask) {
-    string res;
-    for (int i = 0; i < 5; i++) {
+    for (size_t i = 0; i < s.size(); i++) {
         if (mask & (1 << i)) {
-            res += s[i];
+            res *= BASE;
+            res += code(s[i]);
         }
     }
 
@@ -69,6 +62,16 @@ string get(const string& s, int mask) {
 }
 
 int main() {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) { 
+                C[i][j] = 1;
+            } else {
+                C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+            }
+        }
+    }
+
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
@@ -77,31 +80,18 @@ int main() {
     
     cin >> n;
 
+    getline(cin, s[0]);
+
     for (int i = 0; i < n; i++) {
-        cin >> s[i];
+        getline(cin, s[i]);
     }
 
     for (int i = 0; i < n; i++) {
         for (int mask = 0; mask < (1 << 5); mask++) {
-            str[i][mask] = _hash(get(s[i], mask));
-        }
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int mask = 0; mask < (1 << 5); mask++) {
+            str[i][mask] = _hash(s[i], mask);
             cnt[mask][str[i][mask]]++;
         }
     }
-
-    /*
-    for (int mask = 0; mask < (1 << 5); mask++) {
-        printf("%d:", mask);
-        for (auto it: cnt[mask]) {
-            printf(" (%s, %d)", it.first.c_str(), it.second);
-        }
-        puts("");
-    }
-    */
 
     for (int i = 0; i < n; i++) {
         for (int mask = 0; mask < (1 << 5); mask++) {
@@ -109,24 +99,17 @@ int main() {
         }
 
         for (int mask = 0; mask < (1 << 5); mask++) {
-            int omask = fullMask ^ mask;
-            int num = __builtin_popcount(omask);
+            int num = __builtin_popcount(mask);
 
             ans[num] += cnt[mask][str[i][mask]];
-            for (int submask = omask; submask > 0; submask = (submask - 1) & omask) {
-                long long cur = str[i][mask | submask];
-
-                if (__builtin_popcount(submask) % 2 == 0) {
-                    ans[num] += cnt[mask | submask][cur];
-                } else {
-                    ans[num] -= cnt[mask | submask][cur];
-                }
-            }
         }
     }
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 5; i >= 0; i--) {
         cout << ans[i] << ' ';
+        for (int j = i - 1; j >= 0; j--) {
+            ans[j] -= ans[i] * C[i][j];
+        }
     }
     cout << endl;
 
