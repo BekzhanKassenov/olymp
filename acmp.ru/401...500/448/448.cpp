be56c78efd836335dedd683c44cb5f863d348fec
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2015-09-19 23:19:33 +0600

    Solved problem 448 from acmp.ru

--- acmp.ru/401...500/448/448.cpp
+++ acmp.ru/401...500/448/448.cpp
@@ -1,77 +1,64 @@
 #include <iostream>
 #include <cstdio>
-#include <vector>
 #include <cstring>
-#include <set>
 
 using namespace std;
 
 #define all(x) (x).begin(), (x).end()
 
-vector <int> primes;
-bool prime[1500000];
+const int MAXN = 1000010;
 
-void build() {
+bool prime[MAXN];
+int n;
+
+void sieve() {
     memset(prime, true, sizeof prime);
 
     prime[0] = prime[1] = false;
-    for (int i = 2; i < 1500000; i++) {
+    for (int i = 4; i < MAXN; i += 2) {
+        prime[i] = false;
+    }
+    
+    for (int i = 3; i * i < MAXN; i += 2) {
         if (prime[i]) {
-            primes.push_back(i);
-
-            if (1ll * i * i < 1500000) {
-                for (int j = i * i; j < 1500000; j += i) {
-                    prime[j] = false;
-                }
+            for (int j = i * i; j < MAXN; j += i) {
+                prime[j] = false;
             }
         }
     }
 }
 
-int n;
-set <int> Set;
-bool used[500010];
-bool tused[500010];
+void print(int l, int r) {
+    while (l < r) {
+        printf("%d %d\n", l, r);
+        l++, r--;
+    }
+}
+
+void solve(int n) {
+    if (n <= 1) {
+        return;
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        if ((i & 1) != (n & 1) && prime[n + i]) {
+            print(i, n);
+            solve(i - 1);
+            return;
+        }
+    }
+}
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
+    sieve();
+
     scanf("%d", &n);
-
-    for (int i = 1; i <= n; i++) {
-        Set.insert(i);
-    }
-
-    build();
-
-    for (size_t i = 0; Set.size() > 1u && i < primes.size(); i++) {
-        vector <int> temp;
-
-        int num = primes[i];
-
-        for (set <int>::iterator it = Set.begin(); it != Set.end(); it++) {
-            if (*it > num / 2) {
-                break;
-            }
-
-            if (num - *it <= n && 2 * (*it) != num && !used[num - *it] && !tused[*it] && !tused[num - *it]) {
-                printf("%d %d\n", *it, num - *it);
-                temp.push_back(*it);
-                temp.push_back(num - *it);
-
-                tused[*it] = true;
-                tused[num - *it] = true;
-            }
-        }
-
-        for (vector <int>::iterator it = temp.begin(); it != temp.end(); it++) {
-            Set.erase(*it);
-            used[*it] = true;
-
-            tused[*it] = false;
-        }
-    }
-
+    
+    solve(n);
+    
     return 0;
 }
+
