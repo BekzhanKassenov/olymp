c7ae7a5eb10020809b9011067c2b27e6d49b30d8
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2015-09-21 13:42:27 +0600

    Solved problem C from Eastern 2014

--- ACM-ICPC/Quarter Finals/Eastern 2014/C/Main.cpp
+++ ACM-ICPC/Quarter Finals/Eastern 2014/C/Main.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-#define F first
-#define S second
+#define curr first
+#define debt second
 #define MP make_pair
 #define all(x) (x).begin(), (x).end()
 
@@ -19,40 +19,22 @@ const double EPS = 1e-9;
 const double PI = acos(-1.0);
 const int MOD = 1000 * 1000 * 1000 + 7;
 const int INF = 2000 * 1000 * 1000;
-const int MAXN = 1000010;
-const int months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int MAXN = 100010;
 
 template <typename T>
 inline T sqr(T n) {
     return n * n;
 }
 
+int months[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+
 int n, x;
-
-int convert(int day, int month, int hour, int minute) {
-    int ans = 0;
-    for (int i = 0; i < month; i++) {
-        ans += months[i];
-    }
-
-    ans += day;
-
-    ans *= 24;
-    ans += hour;
-    ans *= 60;
-    ans += minute;
-
-    return ans;
-}
-
-/* =================== Letter =====================*/ 
 
 struct Letter {
     int delta;
     int moment;
     int num;
     int treePos;
-    char sign;
 };  
 
 Letter a[MAXN];
@@ -65,11 +47,11 @@ bool cmpPos(const Letter& l1, const Letter& l2) {
     return l1.num < l2.num;
 }
 
-/* =================== Fenwick =====================*/
+pair <long long, long long> tree[4 * MAXN];
 
-int last;
-long long arr[MAXN];
-long long t[4 * MAXN];
+void print(int v) {
+
+}
 
 void update(int v, int l, int r, int pos, int val) {
     if (pos < l || pos > r) {
@@ -77,13 +59,15 @@ void update(int v, int l, int r, int pos, int val) {
     }
 
     if (l == r) {
-        arr[l] += val;
-
-        if (arr[l] < 0) {
-            t[v] = arr[l];
+        if (val < 0) {
+            tree[v].curr = 0;
+            tree[v].debt = -val;
         } else {
-            t[v] = 0;
+            tree[v].curr = val;
+            tree[v].debt = 0;
         }
+
+        print(v);
 
         return;
     }
@@ -92,14 +76,21 @@ void update(int v, int l, int r, int pos, int val) {
     update(2 * v, l, mid, pos, val);
     update(2 * v + 1, mid + 1, r, pos, val);
 
-    t[v] = t[2 * v] + t[2 * v + 1];
+    tree[v].curr = tree[2 * v + 1].curr + max(0ll, tree[2 * v].curr - tree[2 * v + 1].debt);
+    tree[v].debt = -min(0ll, tree[2 * v].curr - tree[2 * v + 1].debt) + tree[2 * v].debt;
+
+    print(v);
 }
 
 int main() {
 #ifdef Local
     freopen("in", "r", stdin);
 #endif
-    
+
+    for (int i = 1; i < 12; i++) {
+        months[i] += months[i - 1];
+    }
+
     scanf("%d\n", &n);
 
     for (int i = 0; i < n; i++) {
@@ -108,43 +99,22 @@ int main() {
         scanf("%d %d.%d %d:%d", &delta, &day, &month, &hour, &minute);
 
         a[i].delta = delta;
-        a[i].moment = convert(day, month, hour, minute);
+        a[i].moment = ((day + months[month - 1]) * 24 + hour) * 60 + minute;
         a[i].num = i;
-
-        if (a[i].delta < 0) {
-            a[i].sign = '-';
-        } else {
-            a[i].sign = '+';
-        }
     }
 
     sort(a, a + n, cmpTime);
 
-    char lastDelta = '+';
-
     for (int i = 0; i < n; i++) {
-        if (a[i].sign == lastDelta) {
-            a[i].treePos = last;
-        } else if (a[i].sign == '+') {
-            last++;
-
-            a[i].treePos = last;
-        }
-
-        lastDelta = a[i].sign;
+        a[i].treePos = i;
     }
 
-    for (int i = 0; i < n; i++) {
-        cout << a[i].moment << ' ' << a[i].sign << endl;
-    }
-
-    last++;
     sort(a, a + n, cmpPos);
 
     for (int i = 0; i < n; i++) {
-        update(1, 0, last - 1, a[i].treePos, a[i].delta);
+        update(1, 0, n - 1, a[i].treePos, a[i].delta);
 
-        printf("%I64d\n", t[1]);
+        printf("%I64d\n", -tree[1].debt);
     }
 
     return 0;
