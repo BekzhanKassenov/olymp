a201b16e7ba5e29f53dd2f975fb06a2f701b2571
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2015-11-03 00:51:56 +0600

    Updated short solution for ACMP 603

--- acmp.ru/601...700/603/short.cpp
+++ acmp.ru/601...700/603/short.cpp
@@ -1,37 +1,35 @@
-#include <fstream>
-    
+#include <iostream>
+     
 #define W while
 #define N 8010
-     
+#define A std::
+      
 char q[N], *p = q + N / 2, *c, *S;
-     
+      
 main() {
-    std::fstream i("input.txt");
-    std::ofstream o("output.txt");
-
-    i.get(q, N);
-    i.get(p, N, 0);
-
+    A cin.get(q, N);
+    A cin.get(p, N, 0);
+ 
     W (*p) {
         c = p, S = q; 
- 
+  
         W (*S) {
             W (*S > 32) {
                 if (*c < 33 | (*c | 32) ^ (*S | 32))
                     goto Q;
-                
+                 
                 S++, c++;
             }
-        
+         
             if (*S++ && *c > 32)
                 goto Q;
-                  
+                   
             W (*c < 33)
                 c++;
         }
- 
-        o << '@';
+  
+        A cout << '@';
         Q:
-        o << *p++;
+        A cout << *p++;
     }
 }
