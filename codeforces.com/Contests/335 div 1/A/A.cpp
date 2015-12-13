57ef94d962e4c12a898ea572dac6f403fe42ec93
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2015-12-13 13:25:00 +0600

    Solved problem A from CF Round 335 div 1

--- codeforces.com/Contests/335 div 1/A/A.cpp
+++ codeforces.com/Contests/335 div 1/A/A.cpp
@@ -19,16 +19,42 @@ const double EPS = 1e-9;
 const double PI = acos(-1.0);
 const int MOD = 1000 * 1000 * 1000 + 7;
 const int INF = 2000 * 1000 * 1000;
+const int MAXN = 100010;
 
 template <typename T>
 inline T sqr(T n) {
     return n * n;
 }
 
+int n;
+int pos[MAXN], len[MAXN];
+
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("in", "r", stdin);
 #endif
 
+    scanf("%d", &n);
+    
+    for (int i = 1; i <= n; i++) {
+        int x;
+        scanf("%d", &x);
+        pos[x] = i;
+    }
+
+    len[1] = 1;
+    int ans = 1;
+    for (int i = 2; i <= n; i++) {
+        len[i] = 1;
+
+        if (pos[i - 1] < pos[i]) {
+            len[i] = len[i - 1] + 1;
+        }
+
+        ans = max(ans, len[i]);
+    }
+
+    printf("%d\n", n - ans);
+    
     return 0;
 }
