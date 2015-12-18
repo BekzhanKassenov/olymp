93beb8a32ba00857ad52907587f5523efd4d0427
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2015-12-18 20:59:36 +0600

    Solved problem E from CF Round 199 div 2

--- codeforces.com/Contests/199 div 2/E/E.cpp
+++ codeforces.com/Contests/199 div 2/E/E.cpp
@@ -9,129 +9,151 @@ using namespace std;
 #define F first
 #define S second
 #define MP make_pair
-#define INF (int)(2e9)
-#define EPS 1e-9
-#define MOD (1000 * 1000 * 1000 + 7)
 #define all(x) (x).begin(), (x).end()
 
-typedef pair <int, int> PII;
-typedef vector <int> VI;
-typedef vector <VI> VVI;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
 
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+const int MOD = 1000 * 1000 * 1000 + 7;
+const int INF = 2000 * 1000 * 1000;
+const int MAXN = 100010;
+const int BSIZE = 330;
+const int LG = 17;
+
 template <typename T>
 inline T sqr(T n) {
-	return (n * n);
+    return n * n;
 }
 
-const int maxn = 100010;
-
-int t, z, n, m, x, y, dist[maxn];
-vector <int> g[maxn];
-
-void dfs(int v, int pr = -1) {
-	if (pr != -1) {
-		if (dist[v] > dist[pr] + 1) {
-			dist[v] = dist[pr] + 1;
-		} else
-			return;
-	}
-
-	for (size_t i = 0; i < g[v].size(); i++) {
-		if (g[v][i] != pr)
-			dfs(g[v][i], v);
-	}
-}
-
-struct que {
- 	PII a[maxn];
-
- 	int head, tail;
-
- 	que() : head(0), tail(0) {}
-
- 	void push(PII n) {
- 		a[tail++] = n;
- 	}
-
- 	void pop() {
- 		head++;
- 	}
-
- 	PII front() {
- 		return a[head];
- 	}
-
- 	bool empty() {
- 		return (head > tail);
- 	}
-
- 	void clear() {
- 		head = tail = 0;
- 	}
+enum CTYPE { 
+    BLUE,
+    RED
 };
 
-que q;
+struct Query {
+    int type;
+    int x;
+};
 
-void bfs(int v) {
-	q.clear();
-	q.push(MP(v, -1));
+Query q[MAXN];
+int n, m;
+vector <int> g[MAXN];
+CTYPE color[MAXN];
+int closest[MAXN];
 
-	while (!q.empty()) {
-		int cur = q.front().F;
-		int pr = q.front().S;
-		q.pop();
+void bfs() {
+    queue <int> q;
+    for (int i = 1; i <= n; i++) {
+        closest[i] = INF;
+        if (color[i] == RED) {
+            q.push(i);
+            closest[i] = 0;
+        }
+    }
 
-		if (pr != -1) {
-			dist[cur] = min(dist[cur], dist[pr] + 1);
-		}
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
 
-		for (size_t i = 0; i < g[cur].size(); i++) {
-			if (g[cur][i] != cur && dist[g[cur][i]] > dist[cur] + 1) {
-				q.push(MP(g[cur][i], cur));
-			}                              
-		}
-	}
+        for (int to: g[v]) {
+            if (closest[to] > closest[v] + 1) {
+                closest[to] = closest[v] + 1;
+                q.push(to);
+            }
+        }
+    }
+}
+
+int anc[LG][MAXN];
+int tin[MAXN], tout[MAXN], timer;
+int dist[MAXN];
+
+void dfs(int v, int par = 1) {
+    tin[v] = timer++;
+    anc[0][v] = par;
+
+    for (int i = 1; i < LG; i++) {
+        anc[i][v] = anc[i - 1][anc[i - 1][v]];
+    }
+
+    for (int to: g[v]) {
+        if (to != par) {
+            dist[to] = dist[v] + 1;
+            dfs(to, v);
+        }
+    }
+
+    tout[v] = timer++;
+}
+
+bool ancestor(int a, int b) {
+    return tin[a] <= tin[b] && tout[b] <= tout[a];
+}
+
+int lca(int a, int b) {
+    if (ancestor(a, b)) {
+        return a;
+    }
+
+    if (ancestor(b, a)) {
+        return b;
+    }
+
+    for (int i = LG - 1; i >= 0; i--) {
+        if (!ancestor(anc[i][a], b)) {
+            a = anc[i][a];
+        }
+    }
+
+    return anc[0][a];
+}
+
+int get_dist(int a, int b) {
+    int l = lca(a, b);
+
+    return dist[a] + dist[b] - 2 * dist[l];
 }
 
 int main() {
-	#ifndef ONLINE_JUDGE
-		freopen("in", "r", stdin);
-	#endif
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+#endif
 
-	scanf("%d%d", &n, &m);
+    scanf("%d%d", &n, &m);
 
-	for (int i = 0; i < n - 1; i++) {
-		scanf("%d%d", &x, &y);
+    for (int i = 0; i < n - 1; i++) {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
 
-		g[--x].push_back(--y);
-		g[y].push_back(x);
-	}
+    color[1] = RED;
+    dfs(1);
+    for (int i = 0; i < m; i++) {
+        if (i % BSIZE == 0) {
+            bfs();
+        }
 
-	memset(dist, INF, sizeof dist);
+        scanf("%d%d", &q[i].type, &q[i].x);
 
-	dfs(0);
+        if (q[i].type == 1) {
+            color[q[i].x] = RED;
+        } else {
+            int ans = closest[q[i].x];
 
-	stringstream ss;
+            for (int j = i - i % BSIZE; j < i; j++) {
+                if (q[j].type == 1) {
+                    ans = min(ans, get_dist(q[i].x, q[j].x));
+                }
+            }
 
-	for (int i = 0; i < m; i++) {
-		scanf("%d%d", &t, &z);
-		z--;
+            printf("%d\n", ans);
+        }
+    }
 
-		if (t == 1) {
-			dist[z] = 0;
-			bfs(z);
-		} else 
-			ss << dist[z] << endl;
-
-		//if (i % 1000 == 0) {
-		//	cout << i << endl;
-		//}
-	}
-
-	cout << ss.str() << endl;
-
-	return 0;
+    return 0;
 }
