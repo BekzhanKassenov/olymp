2ec3c426ac84c43cd6d72604f5c0d90e709a333b
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2015-12-20 00:51:54 +0600

    Updated solution of problem 10 from acmp.ru

--- acmp.ru/001...100/010/10.cpp
+++ acmp.ru/001...100/010/10.cpp
@@ -7,9 +7,9 @@ int main()
 {
 	freopen("input.txt","r",stdin);
 	freopen("output.txt","w",stdout);
-	int a,b,c,d;
+	long long a,b,c,d;
 	cin>>a>>b>>c>>d;
-	for (int x=-100;x<=100;x++)
+	for (long long x=-100;x<=100;x++)
 		if (a*x*x*x+b*x*x+c*x+d==0)
 			cout<<x<<" ";
 	return 0;
