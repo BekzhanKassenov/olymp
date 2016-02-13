dc7b46b963ceda7c59534387e3057da45d076c11
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2016-02-13 11:50:31 +0600

    Updated solution to the problem #45 from acmp.ru

--- acmp.ru/001...100/045/45.cpp
+++ acmp.ru/001...100/045/45.cpp
@@ -3,36 +3,45 @@
 
 using namespace std;
 
-int main()
-{
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-	int n;
-	cin>>n;
-	if (n==0)
-		{
-			cout<<10;
-			return 0;
-		}
-	int a[10];
-	for (int i=0;i<10;i++)
-		a[i]=0;
-	for (int i=9;i>=2;i--)
-	{
-		while (n%i==0)
-		{
-			a[i]++;
-			n/=i;
-		}
-	}
-	if (n!=1)
-		{
-			cout<<-1;
-			return 0;
-		}
-	for (int i=2;i<10;i++)
-		for (int j=1;j<=a[i];j++)
-			cout<<i;
-	return 0;  	
+int n;
+int a[10];
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    cin >> n;
+    
+    if (n == 0) {
+        cout << 10 << endl;
+        return 0;
+    }
+
+    if (1 <= n && n <= 9) {
+        cout << n << endl;
+        return 0;
+    }
+
+    for (int i = 9; i >= 2; i--) {
+        while (n % i == 0) {
+            a[i]++;
+            n /= i;
+        }
+    }
+
+    if (n != 1) {
+        cout << -1 << endl;
+        return 0;
+    }
+    
+    for (int i = 2; i < 10; i++) {
+        for (int j = 1;j <= a[i]; j++) {
+            cout << i;
+        }
+    }
+
+    cout << endl;
+
+    return 0;   
 
 }
