67cb1c422110d0df2e3adafdd43c043ca5c3a605
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2016-03-27 01:05:32 +0600

    Updated solution of the problem 89 from acmp.ru

--- acmp.ru/001...100/088/88.cpp
+++ acmp.ru/001...100/088/88.cpp
@@ -8,61 +8,68 @@ int n;
 
 bool check(vector <int> a)
 {
-	for (int i = 0; i < n * n; i++)
-		if (a[i] != 1)
-			return false;
+    for (int i = 0; i < n * n; i++)
+        if (a[i] != 1)
+            return false;
 
-	return true;
+    return true;
 }
 
 int main()
 {
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
 
-	cin >> n;
+    cin >> n;
 
-	vector <vector <int> > a(n * n, vector <int> (n * n));
+    vector <vector <int> > a(n * n, vector <int> (n * n));
 
-	for (int i = 0; i < n * n; i++)
-		for (int j = 0; j < n * n; j++)
-			cin >> a[i][j];
+    for (int i = 0; i < n * n; i++) {
+        for (int j = 0; j < n * n; j++) {
+            cin >> a[i][j];
 
-	for (int i = 0; i < n * n; i++)
-		{
-			vector <int> a1(n * n), a2(n * n);
+            if (a[i][j] > n * n || a[i][j] < 1) {
+                cout << "Incorrect" << endl;
+                return 0;
+            }
+        }
+    }
 
-			for (int j = 0; j < n * n; j++)
-				{
-					a1[a[i][j] - 1]++;
-					a2[a[j][i] - 1]++;
-				}
+    for (int i = 0; i < n * n; i++)
+        {
+            vector <int> a1(n * n), a2(n * n);
 
-			if (!check(a1) || !check(a2))
-				{
-					cout << "Incorrect";
-					return 0;
-				}	
-		}
+            for (int j = 0; j < n * n; j++)
+                {
+                    a1[a[i][j] - 1]++;
+                    a2[a[j][i] - 1]++;
+                }
 
-	for (int i = 0; i < n * n; i += n)
-		{
-			for (int j = 0; j < n * n; j += n)
-				{
-					vector <int> a1(n * n);
+            if (!check(a1) || !check(a2))
+                {
+                    cout << "Incorrect";
+                    return 0;
+                }   
+        }
 
-					for (int x = i; x < i + n; x++)
-						for (int y = j; y < j + n; y++)
-							a1[a[x][y] - 1]++;
+    for (int i = 0; i < n * n; i += n)
+        {
+            for (int j = 0; j < n * n; j += n)
+                {
+                    vector <int> a1(n * n);
 
-					if (!check(a1))
-						{
-							cout << "Incorrect";
-							return 0;
-						}
-				}
-		}
+                    for (int x = i; x < i + n; x++)
+                        for (int y = j; y < j + n; y++)
+                            a1[a[x][y] - 1]++;
 
-   	cout << "Correct";
-   	return 0;
+                    if (!check(a1))
+                        {
+                            cout << "Incorrect";
+                            return 0;
+                        }
+                }
+        }
+
+    cout << "Correct";
+    return 0;
 }
