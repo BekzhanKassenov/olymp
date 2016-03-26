058546932751010c69fb5c974f47c4e7b18375ea
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2016-03-27 01:00:10 +0600

    Updated solution of problem 87 from acmp.ru

--- acmp.ru/001...100/087/87.cpp
+++ acmp.ru/001...100/087/87.cpp
@@ -1,37 +1,68 @@
 #include <iostream>
+#include <algorithm>
 #include <cstdio>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-int main()
-{
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-	vector <pair <int, string> > q;
-	string s;
-	for (;;)
-		{	
-			cin >> s;
-			if (s == "ENDOFINPUT")
-				break;
-			q.push_back(make_pair(s.length(),s));
-		}
-	int len = q.size();
-	int ans = 0;
-	bool t;
-   for (int k = 0; k < len; k++)
-   	{
-   		t = false;
-   		for (int j = 0; j < len; j++)  
-   			{
-   				for (int i = 0; i < len; i++)
-   					if ((q[i].first + q[j].first == q[k].first) && (q[i].second + q[j].second == q[k].second))
-   						t = true;
-   			}
-   		ans += int(t);
-   	}
-   cout << ans;
-   return 0;
+vector <pair <int, string> > q;
+
+bool equal(int i, int j, int k) {
+    int leni = q[i].first;
+    int lenj = q[j].first;
+    int lenk = q[k].first;
+
+    if (leni + lenj != lenk) {
+        return false;
+    }
+
+    for (int ptr = 0; ptr < leni; ptr++) {
+        if (q[i].second[ptr] != q[k].second[ptr]) {
+            return false;
+        }
+    }
+
+    for (int ptr = 0; ptr < lenj; ptr++) {
+        if (q[j].second[ptr] != q[k].second[leni + ptr]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    string s;
+
+    for (;;) {   
+        cin >> s;
+        if (s == "ENDOFINPUT")
+            break;
+        q.push_back(make_pair(s.length(),s));
+    }
+
+    sort(q.begin(), q.end());
+
+    int len = q.size();
+    int ans = 0;
+    bool t;
+    for (int k = 0; k < len; k++) {
+        t = false;
+
+        for (int j = 0; j < k; j++) {
+            for (int i = 0; i <= j && q[i].first + q[j].first <= q[k].first; i++) {
+                t |= equal(i, j, k) || equal(j, i, k);
+            }
+        }
+
+        ans += t;
+    }
+
+    cout << ans << endl;
+
+    return 0;
 }
