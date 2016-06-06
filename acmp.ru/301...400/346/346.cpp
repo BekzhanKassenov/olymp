4eb4dd0e423ebd6db7cb5bd493fea86426457c09
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2016-06-06 22:04:00 +0600

    Updated solution for problem 346 from acmp.ru

--- acmp.ru/301...400/346/346.cpp
+++ acmp.ru/301...400/346/346.cpp
@@ -5,51 +5,79 @@
 
 using namespace std;
 
-string int_to_str(int a)
-{
-	string ans;
-	while (a)
-		{
-			ans += a % 10;
-			a /= 10;
-		}
-	return ans;
+#define all(x) (x).begin(), (x).end()
+
+string int_to_str(long long a) {
+    string ans;
+    while (a) {
+        ans += a % 10;
+        a /= 10;
+    }
+
+    return ans;
 }
 
-int str_to_int (string s)
-{
-	int ans;
-	for (int i = 0; i < s.length(); i++)
-		{
-			ans *= 10;
-			ans += s[i];
-		}
-	return ans;
+long long str_to_int (const string& s) {
+    long long ans = 0;
+    for (size_t i = 0; i < s.length(); i++) {
+        ans *= 10;
+        ans += s[i];
+    }
+    
+    return ans;
 }
 
-int main()
-{
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	int a, b, c;
-	cin >> a >> b >> c;
-	string stra = int_to_str(a);
-	string strb = int_to_str(b);
-	sort(stra.begin(), stra.end());
-	sort(strb.begin(), strb.end());
-	do
-		{
-			int k = str_to_int(stra);
-			int v = c - k;
-			string str = int_to_str(v);
-			sort(str.begin(), str.end());
-			if (str == strb)
-				{
-					cout << "YES" << endl << k << ' ' << v;
-					return 0;
-				}
-		}
-	while (next_permutation(stra.begin(), stra.end()));
-	cout << "NO";
-	return 0;
+int remove_zeroes(string& s) {
+    int cnt = 0;
+    reverse(all(s));
+
+    while (!s.empty() && s.back() == 0) {
+        cnt++;
+        s.pop_back();
+    }   
+
+    reverse(all(s));
+    return cnt;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    long long a, b, c;
+    cin >> a >> b >> c;
+    
+    string stra = int_to_str(a);
+    string strb = int_to_str(b);
+    
+    sort(stra.begin(), stra.end());
+    sort(strb.begin(), strb.end());
+
+    int cnt0b = remove_zeroes(strb);
+
+    long long ans = -1;
+    long long ansv = -1;
+    do {
+        long long k = str_to_int(stra);
+        long long v = c - k;
+
+        string str = int_to_str(v);
+        sort(str.begin(), str.end());
+        
+        int cnt1 = remove_zeroes(str);
+        if (cnt1 <= cnt0b && str == strb && (ans == -1 || ans > k)) {
+            ans = k;
+            ansv = v;
+        }
+
+    } while (next_permutation(stra.begin(), stra.end()));
+    
+    if (ans == -1) {
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+        cout << ans << ' ' << ansv << endl;
+    }
+
+    return 0;
 }
