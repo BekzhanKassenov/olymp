9f43f96b3b9604ef976057ac97ef45f62b1b8bfc
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2016-06-06 19:49:21 +0600

    Solved problem 671 from acmp.ru

--- acmp.ru/601...700/671/671.cpp
+++ acmp.ru/601...700/671/671.cpp
@@ -1,38 +1,68 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
 
-long long n;
-int ans;
+typedef long long uint;
+
+const int MAXN = 35;
+
+char digit[2] = {'4', '7'};
+
+bool calced[MAXN][2];
+uint dp[MAXN][2];
+char n[MAXN];
+int len;
+
+uint f(int pos, bool smaller) {
+    if (calced[pos][smaller]) {
+        return dp[pos][smaller];
+    }
+
+    calced[pos][smaller] = true;
+    uint& ans = dp[pos][smaller];
+
+    if (pos == len) {
+        return ans = 1;
+    }
+
+    if (smaller) {
+        return ans = 1LL << (len - pos);
+    }
+
+    int upper = -1;
+    if (n[pos] >= '7') {
+        upper = 1;
+    } else if (n[pos] >= '4') {
+        upper = 0;
+    }
+
+    for (int d = 0; d <= upper; d++) {
+        if (digit[d] < n[pos]) {
+            ans += f(pos + 1, true);
+        } else {
+            ans += f(pos + 1, false);
+        }
+    }
+
+    return ans;
+}
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    scanf("%I64d", &n);
+    gets(n);
+    len = strlen(n);
 
-    for (int len = 1; len < 11; len++) {
-        for (int mask = 0; mask < (1 << len); mask++) {
-            long long num = 0;
-
-            for (int i = 0; i < len; i++) {
-                num *= 10;
-
-                if (mask & (1 << i)) {
-                    num += 4;
-                } else {
-                    num += 7;
-                }
-            }
-
-            if (num <= n) {
-                ans++;
-            }
-        }
+    uint ans = f(0, false);
+    
+    for (int i = 1; i < len; i++) {
+        ans += 1ll << i;
     }
 
-    printf("%d\n", ans);
+    printf("%lld\n", ans);
 
     return 0;
 }
