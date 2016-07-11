4c93ec8c0886f2ff564a761c48448e0d99202a3e
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2016-07-11 23:18:51 +0200

    Solved problem 1095 from acm.timus.ru

--- acm.timus.ru/Том 01(1000 - 1099)/1095/1095.cpp
+++ acm.timus.ru/Том 01(1000 - 1099)/1095/1095.cpp
@@ -1,23 +1,62 @@
-#include <iostream>
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
+int ans[7] = {4312, 3214, 4321, 4231, 2314, 3421, 4213};
+char s[25];
+int n;
+
 int main() {
-	#ifndef ONLINE_JUDGE
-		freopen("in", "r", stdin);
-	#endif
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+#endif
 
-	int n;
+    scanf("%d\n", &n);
 
-	cin >> n;
+    while (n--) {
+        gets(s);
+        int len = strlen(s);
 
-	string s;
+        for (char c = '1'; c <= '4'; c++) {
+            auto it = find(s, s + len, c);
+            swap(*it, s[len - 1]);
+            len--;
+        }
 
-	for (int i = 0; i < n; i++) {
-		cin >> s;
+        s[len] = '\0';
 
-	}
+        if (s[0] == '0') {
+            int pos = -1;
+            for (int i = 0; i < len; i++) {
+                if (s[i] != '0') {
+                    pos = i;
+                    break;
+                }
+            }
 
-	return 0;
+            if (pos == -1) {
+                printf("4312%s\n", s);
+                continue;
+            } else {
+                swap(s[0], s[pos]);
+            }
+        }
+
+        int rem = 0;
+        for (int i = 0; i < len; i++) {
+            rem *= 10;
+            rem += s[i] - '0';
+            rem %= 7;
+        }
+
+        rem *= 10000;
+        rem %= 7;
+        rem = (7 - rem) % 7;
+        
+        printf("%s%d\n", s, ans[rem]);
+    }
+
+    return 0;
 }
