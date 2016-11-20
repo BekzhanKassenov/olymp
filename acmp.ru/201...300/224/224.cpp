29fdd0aaef3ddf4ef52417c402b222fa15ec88d2
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2016-11-20 15:34:23 +0100

    Updated solution for problem 224 from acmp.ru

--- acmp.ru/201...300/224/224.cpp
+++ acmp.ru/201...300/224/224.cpp
@@ -1,62 +1,43 @@
-#include <iostream>
-#include <cstdio>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-	freopen ("input.txt","r",stdin);
-	freopen ("output.txt","w",stdout);
-	int n;
-	cin >> n;
-	long long s1 , s2 , s3 , s4 , s5;
-		{
-			long long z1 , z2 , z3;
-			cin >> z1 >> z2 >> z3;
-			s1 = max ( max ( z1 , z2 ) , z3);
-			s3 = min ( min ( z1 , z2 ) , z3);
-			s2 = z1 + z2 + z3 - s1 - s3;
-			s4 = s2;
-			s5 = s3;
-		}
-	for (int i = 3 ;i < n; i++)
-		{
-			long long z;
-			cin >> z;
-			if (z > s1)
-				{
-					s3 = s2;
-					s2 = s1;
-					s1 = z;
-				}	
-			else 
-				{
-					if (z > s2)
-						{
-							s3 = s2;
-							s2 = z;
-						}
-					else
-						{
-							if (z > s3)
-								{	
-									s3 = z;
-								}
-						}
-			   }
-			if (z < s5)
-				{
-					s4 = s5;
-					s5 = z;
-				}
-			else
-				{
-					if (z < s4)
-						{
-							s4 = z;
-						}
-			   }
-		}
-	cout << max( s1 * s2 * s3 , s1 * s4 * s5);
-	return 0;
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+    int n;
+    scanf("%d", &n);
+
+    vector <int> a(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    sort(a.begin(), a.end());
+
+    vector <long long> vec;
+    for (int i = 0; i < 3 && !a.empty(); i++) {
+        vec.push_back(a.back());
+        a.pop_back();
+    }
+
+    for (int i = 0; i < min(3, (int)a.size()); i++) {
+        vec.push_back(a[i]);
+    }
+
+    long long ans = -1e18;
+    for (size_t i = 0; i < vec.size(); i++) {
+        for (size_t j = i + 1; j < vec.size(); j++) {
+            for (size_t k = j + 1; k < vec.size(); k++) {
+                ans = max(ans, vec[i] * vec[j] * vec[k]);
+            }
+        }
+    }
+
+    printf("%I64d\n", ans);
+
+    return 0;
 }
