47198f3146d1ac7250924713c47a112808d3a230
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2016-11-21 22:21:44 +0100

    Solved problem 546 from acmp.ru

--- acmp.ru/501...600/546/546.cpp
+++ acmp.ru/501...600/546/546.cpp
@@ -4,12 +4,44 @@
 using namespace std;
 
 int n;
+pair <int, int> front[510], back[510];
 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
     scanf("%d", &n);    
+    int limit = n + (4 - n % 4) % 4;
+
+    int num = 1;
+    int size = limit / 4;
+    front[1].second = num;
+    num++;
+
+    for (int i = 1; i <= size && num <= n; i++) {
+        back[i].first = num;
+        num++;
+
+        if (num <= n) {
+            back[i].second = num;
+            num++;
+        }
+    }
+
+    for (int i = size; i > 0 && num <= n; i--) {
+        front[i].first = num;
+        num++;
+
+        if (num <= n) {
+            front[i].second = num;
+            num++;
+        }
+    }
+
+    for (int i = 1; i <= size; i++) {
+        printf("%d %d %d %d\n", i, 1, front[i].first, front[i].second);
+        printf("%d %d %d %d\n", i, 2, back[i].first, back[i].second);
+    }
 
     return 0;
 }
