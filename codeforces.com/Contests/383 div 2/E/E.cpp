7d1c0de69a36be4ce813ef04bb575230aeef9ff9
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2016-12-07 03:09:12 +0600

    Solved problem E from CF Round 383 div 2

--- codeforces.com/Contests/383 div 2/E/E.cpp
+++ codeforces.com/Contests/383 div 2/E/E.cpp
@@ -19,16 +19,58 @@ const double EPS = 1e-9;
 const double PI = acos(-1.0);
 const int MOD = 1000 * 1000 * 1000 + 7;
 const int INF = 2000 * 1000 * 1000;
+const int MAXN = 200010;
 
 template <typename T>
 inline T sqr(T n) {
     return n * n;
 }
 
+int n;
+vector <int> g[MAXN];
+int a[MAXN], b[MAXN];
+int color[MAXN];
+
+void dfs(int v, int c = 1) {
+    color[v] = c;
+    for (int to : g[v]) {
+        if (color[to] == 0) {
+            dfs(to, 3 - c);
+        }
+    }
+}
+
 int main() {
 #ifndef ONLINE_JUDGE
     freopen("in", "r", stdin);
 #endif
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        int u = 2 * i - 1;
+        int v = 2 * i;
+
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%d%d", a + i, b + i);
+
+        g[a[i]].push_back(b[i]);
+        g[b[i]].push_back(a[i]);
+    }
+
+    for (int i = 1; i <= 2 * n; i++) {
+        if (color[i] == 0) {
+            dfs(i);
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d %d\n", color[a[i]], color[b[i]]);
+    }
 
     return 0;
 }
