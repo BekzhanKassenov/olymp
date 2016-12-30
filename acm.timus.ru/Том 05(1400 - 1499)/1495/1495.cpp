a817b3ff468bafb315b63ebc0d4c4dc98e24e737
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2016-12-30 20:06:17 +0600

    Solved problem 1495 from acm.timus.ru

--- acm.timus.ru/Том 05(1400 - 1499)/1495/1495.cpp
+++ acm.timus.ru/Том 05(1400 - 1499)/1495/1495.cpp
@@ -1,46 +1,79 @@
-#include <iostream>
-#include <cstdio>
-#include <vector>
-#include <cstring>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-bool is_ok(int n) {
-	if (n == 0)
-		return false;
+const int MAXN = 1000010;
 
-	while (n) {
-		if ((n % 10 != 1) && (n % 10 != 2))
-			return false;
-
-		n /= 10;
-	}
-
-	return true;
-}
-
-int ans[100010];
+int n;
+bool used[MAXN];
+int len[MAXN];
+int par[MAXN];
+int parn[MAXN];
+int ans[MAXN];
 
 int main() {
-	freopen("in", "r", stdin);
+#ifndef ONLINE_JUDGE
+    freopen("in", "r", stdin);
+#endif
 
-	for (int i = 0; i < 100000; i++)
-		ans[i] = 1000000;
+    scanf("%d", &n);
 
-	for (int i = 0; i < 100000; i++) {
-	 	if (is_ok(i)) {
-	 		int c = i;
+    if (n == 1) {
+        puts("1");
+        return 0;
+    }
 
-	 		for (int j = 2; j * j <= c; j++) {
-	 			if (c % j == 0) {
-	 				ans[j] = min(ans[j], c);
+    if (n == 2) {
+        puts("2");
+        return 0;
+    }
 
-	 				ans[c / j] = min(ans[c / j], c);
-	 			}
-	 		}
-	 	}
-	}
+    queue <int> q;
+    len[1] = 1;
+    len[2] = 1;
+    par[1] = -1;
+    par[2] = -1;
+    parn[1] = 1;
+    parn[2] = 2;
+    used[1] = true;
+    used[2] = true;
+    q.push(1);
+    q.push(2);
 
-	for (int i = 0; i < 15; i++)
-		cout << i << ' ' << ans[i] << endl;
+    while (!q.empty()) {
+        int m = q.front();
+        q.pop();
+        if (m == 0) {
+            break;
+        }
+
+        for (int i = 1; i <= 2; i++) {
+            int m1 = (m * 10 + i) % n;
+            if (!used[m1] || (len[m1] == len[m] + 1 && parn[m] == 1 && parn[par[m1]] == 2)) {
+                used[m1] = true;
+                len[m1] = len[m] + 1;
+                par[m1] = m;
+                parn[m1] = i;
+                q.push(m1);
+            }
+        }
+    }
+
+    if (!used[0]) {
+        puts("Impossible");
+        return 0;
+    }
+
+    int v = 0;
+    int ansl = 0;
+    do {
+        ans[ansl++] = parn[v];
+        v = par[v];
+    } while (v != -1);
+
+    for (int i = ansl - 1; i >= 0; i--) {
+        printf("%d", ans[i]);
+    }
+    puts("");
+    return 0;
 }
