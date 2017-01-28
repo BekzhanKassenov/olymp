bf5faebad1c90b10dfcf0f5bb1540ba56a885b8d
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2017-01-28 14:33:32 +0600

    Solved problem B from CF Round 392 div 2

--- codeforces.com/Contests/392 div 2/B/B.cpp
+++ codeforces.com/Contests/392 div 2/B/B.cpp
@@ -28,56 +28,6 @@ inline T sqr(T n) {
 
 int n;
 char s[MAXN];
-int ans[256];
-
-int cnt(int l) {
-    if (l < 0 || l + 3 >= n) {
-        return -1;
-    }
-
-    int ans = 0;
-    for (int i = 0; i < 4; i++) {
-        if (s[i + l] == '!') {
-            ans++;
-        }
-    }
-
-    return ans;
-}
-
-bool fix(int l) {
-    int pos = -1;
-    for (int i = 0; i < 4; i++) {
-        if (s[i + l] == '!') {
-            pos = l + i;
-            break;
-        }
-    }
-
-    if (pos == -1) {
-        return false;
-    }
-
-    for (char c : { 'R', 'B', 'Y', 'G'} ) {
-        bool ok = true;
-        for (int i = 0; i < 4; i++) {
-            if (s[i + l] == c) {
-                ok = false;
-                break;
-            }
-        }
-
-        if (ok) {
-            ans[c]++;
-            s[pos] = c;
-            break;
-        }
-    }
-
-    return true;
-}
-
-bool used[MAXN];
 
 int main() {
 #ifndef ONLINE_JUDGE
@@ -87,33 +37,25 @@ int main() {
     gets(s);
     n = strlen(s);
 
-    queue <int> q;
-    for (int i = 0; i < n; i++) {
-         if (cnt(i) == 1) {
-            q.push(i);
-            used[i] = true;
-         }
-    }
-
-    while (!q.empty()) {
-        int v = q.front();
-        q.pop();
-
-        if (!fix(v)) {
-            continue;
-        }
-
-        for (int i = v - 5; i < v + 5; i++) {
-            if (i >= 0 && cnt(i) == 1 && !used[i]) {
-                used[i] = true;
-                q.push(i);
+    for (char c : {'R', 'B', 'Y', 'G'}) {
+        int pos = -1;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == c) {
+                pos = i;
+                break;
             }
         }
-    }
 
-    for (char c : { 'R', 'B', 'Y', 'G'}) {
-        printf("%d ", ans[c]);
+        int ans = 0;
+        for (int i = pos % 4; i < n; i += 4) {
+            if (s[i] == '!') {
+                ans++;
+            }
+        }
+
+        printf("%d ", ans);
     }
+    puts("");
 
     return 0;
 }
