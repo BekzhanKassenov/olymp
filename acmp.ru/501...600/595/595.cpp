061135440cd564cb2e18fb6cf902c146a2eaa929
BekzhanKassenov <bekzhan.kassenov@nu.edu.kz>
2017-03-11 20:02:25 +0600

    Solved problem 595 from acmp.ru

--- acmp.ru/501...600/595/595.cpp
+++ acmp.ru/501...600/595/595.cpp
@@ -1,12 +1,43 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
 
 const int MAXN = 50010;
 
 char s[MAXN], t[MAXN];
-int n, ans = -1;
+int n;
+int z[2 * MAXN];
+
+struct proxy {
+    const char *s, *t;
+    int slen, tlen;
+
+    proxy(char s[], char t[]) {
+        this->s = s;
+        this->t = t;
+        slen = strlen(s);
+        tlen = strlen(t);
+    }
+
+    char operator [](int pos) const {
+        if (pos < tlen) {
+            return t[pos];
+        }
+
+        if (pos == tlen) {
+            return '#';
+        }
+
+        pos -= tlen + 1;
+        return s[pos];
+    }
+
+    int size() const {
+        return slen + tlen + 1;
+    }
+};
 
 int main() {
     freopen("input.txt", "r", stdin);
@@ -17,29 +48,50 @@ int main() {
 
     n = strlen(s);
 
-    int l = 0, r = n - 1;
-    int dl = 1;
-
-    for (int i = 0; i < n; i++) {
-        if (s[l] != t[r]) {
-            if (ans != -1) {
-                puts("No");
-                return 0;
-            }
-
-            ans = i;
-            cout << ans << endl;
-            l = n;
-            dl = -1;
-        }
-        r--;
-        l += dl;
+    if (strcmp(s, t) == 0) {
+        puts("Yes");
+        puts("0");
+        return 0;
     }
 
-    if (ans == -1)
-        ans = n;
+    proxy p(s, t);
 
-    printf("%d\n", ans);
+    z[0] = 1;
+    for (int i = 1, l = 0, r = 0; i < p.size(); i++) {
+        if (i <= r) {
+            z[i] = min(z[i - l], r - i + 1);
+        }
+
+        while (i + z[i] < p.size() && p[z[i]] == p[i + z[i]]) {
+            z[i]++;
+        }
+
+        if (i + z[i] - 1 > r) {
+            l = i, r = i + z[i] - 1;
+        }
+    }
+
+    /*
+    for (int i = 0; i <= 2 * n; i++) {
+        cout << p[i];
+    }
+    cout << endl;
+    for (int i = 0; i <= 2 * n; i++) {
+        cout << z[i];
+    }
+    cout << endl;
+    */
+
+    for (int k = 0; k < n && s[k] == t[n - 1 - k]; k++) {
+        int idx = k + 1;
+        if (z[n + idx + 1] == n - k - 1) {
+            puts("Yes");
+            printf("%d\n", k + 1);
+            return 0;
+        }
+    }
+
+    puts("No");
 
     return 0;   
 }
