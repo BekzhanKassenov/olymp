b2b3861b7a7b2d1583afbbeea763989e357f7154
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2017-06-26 17:46:11 +0600

    Updated solution of problem 82 from acmp.ru

--- acmp.ru/001...100/082/82.cpp
+++ acmp.ru/001...100/082/82.cpp
@@ -1,30 +1,45 @@
-#include <iostream>
-#include <cstdio>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-	int n,m;
-	cin>>n>>m;
-	int used[100010];
-	int z;
-	for (int i=0;i<100010;i++)
-			used[i]=0;
-	for (int i=0;i<n;i++)
-		{
-			cin>>z;
-			used[z]=1;
-		}
-	for (int i=0;i<m;i++)
-		{
-			cin>>z;
-			if (used[z]==1)
-			used[z]=2;
-		}
-	for (int i=0;i<100010;i++)
-		if (used[i]==2) cout<<i<<" ";
-	return 0;
+#define all(x) (x).begin(), (x).end()
+
+int read_int() {
+    int x;
+    scanf("%d", &x);
+    return x;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int n = read_int();
+    int m = read_int();
+
+    auto read_vec = [](int n) -> vector <int> {
+        vector <int> res(n);
+        for (int i = 0; i < n; i++) {
+            res[i] = read_int();
+        }
+
+        sort(all(res));
+        res.resize(unique(all(res)) - res.begin());
+    
+        return res;
+    };
+
+    vector <int> a(read_vec(n));
+    vector <int> b(read_vec(m));
+
+    for (int x : a) {
+        if (binary_search(all(b), x)) {
+            printf("%d ", x);
+        }
+    }
+
+    puts("");
+    return 0;
 }
