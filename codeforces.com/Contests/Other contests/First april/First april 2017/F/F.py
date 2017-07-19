8af4bb563c7d6d6f43410eb988000e1cef9bf341
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2017-07-19 17:41:20 +0600

    Solved problem F from First April 2017 contest

--- codeforces.com/Contests/Other contests/First april/First april 2017/F/F.py
+++ codeforces.com/Contests/Other contests/First april/First april 2017/F/F.py
@@ -1,2 +1,5 @@
-for i in sorted(list(map(lambda x : int(x), input().split()))[1:]): print(i, end=" ")
+import time
 
+now = time.time()
+while time.time() < (now+0.987): pass
+for i in sorted(list(map(lambda x : int(x), input().split()))[1:]): print(i, end=" ")
