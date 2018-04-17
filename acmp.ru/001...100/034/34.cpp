4f47c32104257576d8e1e3e9beed526ca02eebd1
Bekzhan Kassenov <bekzhan.kassenov@gmail.com>
2018-04-17 19:23:02 +0200

    Updated solution to problem 34 from acmp.ru

--- acmp.ru/001...100/034/34.cpp
+++ acmp.ru/001...100/034/34.cpp
@@ -1,49 +1,29 @@
 #include <iostream>
 #include <cstdio>
-#include <vector>
+#include <set>
 #include <string>
+
 using namespace std;
 
-int n,k;
-string s;
+set <string> st;
 
-int stp1(int k)
-{
-	int ans=1;
-	for (int i=0;i<k;i++)
-		ans*=10;
-	return ans;
-}
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 
-int val(string s)
-{
-	int st=s.length();
-	int ans=0;
-	while (st>0)
-		{
-			ans+=(s[s.length()-st]-'0')*stp1(st-1);
-			st--;
+	int n, k;
+	cin >> n >> k;
+
+	string s;
+	cin >> s;
+	
+	for (int i = 0; i + k <= n; i++) {
+		if (!st.insert(s.substr(i, k)).second) {
+			puts("YES");
+			return 0;
 		}
-	return ans;
-}
+	}
 
-int main()
-{
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
-	cin>>n>>k;
-	cin>>s;
-	vector <int> a;
-	a.resize(stp1(k));
-	int len=s.length();
-	for (int i=0;i<len-k;i++)
-		a[val(s.substr(i,k))]++;
-   for (int i=0;i<stp1(k);i++)
-   	if (a[i]>=2)
-   		{
-   			cout<<"YES";
-   			return 0;
-   		}
-   cout<<"NO";
+	puts("NO");
 	return 0;
 }
