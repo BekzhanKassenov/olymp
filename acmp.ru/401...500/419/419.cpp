245ee94a4d8719f75ca9bc6481087bfda9a7a15f
BekzhanKassenov <bekzhan.kassenov@gmail.com>
2019-05-10 01:13:13 +0200

    Updated solution of problem 419 from acmp.ru

--- acmp.ru/401...500/419/419.cpp
+++ acmp.ru/401...500/419/419.cpp
@@ -3,76 +3,85 @@
 #include <cstring>
 #include <string>
 #include <cctype>
-
+#include <map>
+ 
 using namespace std;
-
+ 
 const int MAXN = 100100;
-
+ 
 char s[MAXN], temp[MAXN];
 int n;
-
+ 
 inline void erase_first(char s[]) {
- 	for (int i = 0; s[i]; i++) {
- 	 	s[i] = s[i + 1];
- 	}
+    for (int i = 0; s[i]; i++) {
+        s[i] = s[i + 1];
+    }
 }
 
+map <pair <int, int>, bool> mem;  
 inline bool simple_check(int l, int r) {
- 	while (l < r) {
- 		if (s[l] != s[r]) {
- 		 	return false;
- 		}
+    pair <int, int> entry = make_pair(l, r);
+    if (mem.count(entry)) {
+        return mem[entry];
+    }
 
- 		l++, r--;
- 	}
+    bool& ans = mem[entry];
+    if (l >= r) {
+        return ans = true;
+    }
 
- 	return true;
+    if (s[l] == s[r]) {
+        return ans = simple_check(l + 1, r - 1);
+    }
+
+    return false;
 }
 
 inline bool palindrome(int l, int r) {
- 	if (l >= r)
- 		return true;
-
- 	if (s[l] == s[r] && palindrome(l + 1, r - 1))
- 		return true;
-
- 	if (simple_check(l, r - 1)) {
- 	 	erase_first(s + r);
- 	 	return true;
- 	}
-
- 	if (simple_check(l + 1, r)) {
- 	 	erase_first(s + l);
- 	 	return true;
- 	}
-
- 	if (simple_check(l + 1, r - 1)) {
- 	 	s[l] = s[r];
- 	 	return true;
- 	}
-
- 	return false;
+    if (l >= r)
+        return true;
+ 
+    if (s[l] == s[r] && palindrome(l + 1, r - 1)) {
+        return true;
+    }
+ 
+    if (simple_check(l, r - 1)) {
+        erase_first(s + r);
+        return true;
+    }
+ 
+    if (simple_check(l + 1, r)) {
+        erase_first(s + l);
+        return true;
+    }
+ 
+    if (simple_check(l + 1, r - 1)) {
+        s[l] = s[r];
+        return true;
+    }
+ 
+    return false;
 }
-
+ 
 int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-
-	gets(temp);
- 	for (int i = 0; temp[i]; i++)  {	
-     	if (isalpha(temp[i])) {
-     	 	s[n++] = toupper(temp[i]);
-     	}
+ 
+    fgets(temp, MAXN, stdin);
+    for (int i = 0; temp[i]; i++)  {    
+        if (isalpha(temp[i])) {
+            s[n++] = toupper(temp[i]);
+        }
     }
-
+ 
     s[n] = '\0';
-
+ 
     if (palindrome(0, n - 1)) {
-     	puts("YES");
-     	puts(s);
+        puts("YES");
+        puts(s);
     } else {
-		puts("NO");
+        puts("NO");
     }
-
+ 
     return 0;
 }
