95f48a911105170cdee63c058bda991c02f40b45
Bekzhan Kassenov <bekzhan.kassenov@gmail.com>
2020-12-07 22:45:42 +0100

    Solved problem E from 2015 Summer KFU Programming Contest (CF id: 100778)

--- codeforces.com/Gym/2015 Indian Summer KFU Programming Contest/E/Main.cpp
+++ codeforces.com/Gym/2015 Indian Summer KFU Programming Contest/E/Main.cpp
@@ -19,104 +19,106 @@ const double EPS = 1e-9;
 const double PI = acos(-1.0);
 const int MOD = 1000 * 1000 * 1000 + 7;
 const int INF = 2000 * 1000 * 1000;
-const int MAXN = 100010;
+const int MAXN = 120010;
+const int BSIZE = 340;
 
 template <typename T>
 inline T sqr(T n) {
     return n * n;
 }
 
-struct Node;
+int n;
+int a[MAXN];
+int block[BSIZE][26];
 
-typedef Node* pNode;
-
-enum ETYPE {
-    OPEN,
-    LETTER,
-    CLOSE
+void reset(int bnum) {
+    for (int i = 0; i < 26; i++) {
+        block[bnum][i] = i;
+    }
 }
 
-struct Event {
-    ETYPE type;
-    int pos, num;
-    char x, y;
-};
-
-pNode table[26][26];
-char s[MAXN];
-char ans[MAXN];
-int n, m;
-
-void add(char fr, char to, int num) {
-    char ret = to;
-    int diff = INF;
-
-    int idxfr = fr - 'a';
-    int idxto = to - 'a';
+void apply_to_block(int row, int from, int to) {
     for (int i = 0; i < 26; i++) {
-        pNode temp = getBest(table[idxto][i], num);
-
-        if (temp != NULL && temp.num - num < diff) {
-            diff = temp.num - num;
-            ret = temp.ret;
+        if (block[row][i] == from) {
+            block[row][i] = to;
         }
     }
-
-    pNode item = new Node(num, ret);
-    table[idxfr][idxto].insert(item);
 }
 
-void erase(char fr, char to, int num) {
-
+void flush_block(int bnum) {
+    for (int i = BSIZE * bnum; i < BSIZE * (bnum + 1); i++) {
+        a[i] = block[bnum][a[i]];
+    }
+    reset(bnum);
 }
 
-char getLetter(char old) {
-
+void naive(int l, int r, int from, int to) {
+    for (int i = l; i <= r; i++) {
+        if (a[i] == from) {
+            a[i] = to;
+        }
+    }
 }
 
 int main() {
 #ifdef Local
     freopen("in", "r", stdin);
 #endif
+    
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i = 0; i < n; i++) {
+        a[i] = s[i] - 'a';
+    }
 
-    scanf("%d\n", &n);
-    gets(s + 1);
+    for (int i = 0; i < BSIZE; i++) {
+        reset(i);
+    }
 
-    vector <Event> events;
-    scanf("%d\n", &m);
-    for (int i = 0; i < m; i++) {
+    int Q;
+    cin >> Q;
+
+    while (Q--) {
         int l, r;
-        char a, b;
+        char cfrom, cto;
+        cin >> l >> r >> cfrom >> cto;
+        int from = cfrom - 'a';
+        int to = cto - 'a';
+        l--, r--;
 
-        scanf("%d %d %c %c\n", &l, &r, &a, &b);
+        int lblock = l / BSIZE;
+        int rblock = r / BSIZE;
+        if (lblock == rblock) {
+            flush_block(lblock);
+            naive(l, r, from, to);
+            continue;
+        }
 
-        events.push_back({OPEN, l, i, a, b});
-        events.push_back({CLOSE, r, i, a, b});
-    }
+        if (l % BSIZE != 0) {
+            flush_block(lblock);
+            naive(l, BSIZE * (lblock + 1) - 1, from, to);
+            lblock++;
+        }
+        if (r % BSIZE != BSIZE - 1) {
+            flush_block(rblock);
+            naive(BSIZE * rblock, r, from, to);
+            rblock--;
+        }
 
-    for (int i = 1; i <= n; i++) {
-        events.push_back({LETTER, i, 0, s[i], '\0'});
-    }
-
-    sort(all(events));
-
-    for (const auto& event: events) {
-        switch (event.type) {
-            case OPEN:
-                add(events[i].x, events[i].y, events[i].num);
-                break;
-
-            case CLOSE:
-                erase(events[i].x, events[i].y, events[i].num);
-                break;
-
-            case LETTER:
-                ans[events[i].pos] = getLetter(events[i].x);
-                break;
+        for (int bnum = lblock; bnum <= rblock; bnum++) {
+            apply_to_block(bnum, from, to);
         }
     }
 
-    puts(ans + 1);
-    
+    for (int i = 0; i < BSIZE; i++) {
+        flush_block(i);
+    }
+
+    for (int i = 0; i < n; i++) {
+        putchar(a[i] + 'a');
+    }
+
+    puts("");
     return 0;
 }
