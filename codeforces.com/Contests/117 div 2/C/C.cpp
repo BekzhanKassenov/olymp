c1fbe9fe1674f9527f50c7c77e2a9592038174fb
Bekzhan Kassenov <bekzhan.kassenov@gmail.com>
2020-12-18 13:33:52 +0100

    Solved problem C from CF Round 117 Div 2

--- codeforces.com/Contests/117 div 2/C/C.cpp
+++ codeforces.com/Contests/117 div 2/C/C.cpp
@@ -18,26 +18,133 @@
 #include <limits.h>
 #include <ctime>
 
-
 using namespace std;
 
-#define abs(x) (x < 0) ? (-x) : x
-#define sqr(x) (x) * (x)
-#define INF 2000000000
-#define EPS 10e-14
-#define PII pair <int, int>
-#define F first
-#define S second
-#define MP make_pair
-#define PB push_back
-#define VI vector <int>
-#define LL long long
-#define ULL unsigned long long
+const int MAXN = 100010;
+const long long INF = 1000ll * 1000 * 1000 * 1000 * 1000 * 1000;
 
-int main()
-{
+struct Sum {
+    int k;
+    multiset <int> kmax;
+    long long kmaxsum = 0;
+    multiset <int> rest;
+    long long restsum = 0;
+
+    Sum(int k) : k(k) {}
+
+    void rm_kmax(multiset<int>::iterator it) {
+        kmaxsum -= *it;
+        kmax.erase(it);
+    }
+
+    void rm_rest(multiset<int>::iterator it) {
+        restsum -= *it;
+        rest.erase(it);
+    }
+
+    void add_kmax(int num) {
+        kmax.insert(num);
+        kmaxsum += num;
+    }
+
+    void add_rest(int num) {
+        rest.insert(num);
+        restsum += num;
+    }
+
+    void add(int num) {
+        if (k == 0) {
+            add_rest(num);
+            return;
+        }
+        if ((int)kmax.size() < k) {
+            add_kmax(num);
+            return;
+        }
+        auto mn = kmax.begin();
+        int mnval = *mn;
+        if (num > mnval) {
+            rm_kmax(mn);
+            add_rest(mnval);
+            add_kmax(num);
+        } else {
+            add_rest(num);
+        }
+    }
+
+    void rm(int num) {
+        auto restit = rest.find(num);
+        if (restit != rest.end()) {
+            rm_rest(restit);
+            return;
+        }
+        rm_kmax(kmax.find(num));
+        if (!rest.empty()) {
+            auto mx = --rest.end();
+            int mxval = *mx;
+            rm_rest(mx);
+            add_kmax(mxval);
+        }
+    }
+};
+
+int n, len, k;
+int a[MAXN];
+
+int main() {
 	#ifndef ONLINE_JUDGE
 		freopen("in", "r", stdin);
 	#endif
+
+    cin >> n >> len;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    cin >> k;
+
+    Sum pos(k), neg(k);
+
+    for (int i = 1; i <= len; i++) {
+        if (a[i] >= 0) {
+            pos.add(a[i]);
+        } else {
+            neg.add(-a[i]);
+        }
+    }
+
+    long long ans = -INF;
+
+    auto solve = [&]() {
+        long long posmax = pos.kmaxsum;
+        long long posrest = pos.restsum;
+        long long negmax = neg.kmaxsum;
+        long long negrest = neg.restsum;
+
+        long long S1 = abs(posmax + posrest - negmax - negrest);
+        long long S2 = abs(-posmax + posrest - negmax - negrest);
+        long long S3 = abs(posmax + posrest + negmax - negrest);
+
+        ans = max(ans, max(S1, max(S2, S3)));
+    };
+
+    solve();
+    for (int i = len + 1; i <= n; i++) {
+        if (a[i] >= 0) {
+            pos.add(a[i]);
+        } else {
+            neg.add(-a[i]);
+        }
+        if (a[i - len] >= 0) {
+            pos.rm(a[i - len]);
+        } else {
+            neg.rm(-a[i - len]);
+        }
+        solve();
+    }
+
+    cout << ans << endl;
+
+
 	return 0;
 }
