43297f1010ea1891c3816a35d1e90dd93c461f51
Bekzhan Kassenov <bekzhan.kassenov@gmail.com>
2021-08-29 11:39:55 +0200

    Updated solution for problem 140 from acmp.ru

--- acmp.ru/101...200/140/140.cpp
+++ acmp.ru/101...200/140/140.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+const int INF = 2000000000;
+
 int main()
 {
 	freopen("input.txt","r",stdin);
@@ -12,14 +14,18 @@ int main()
 	cin >> n;
 	vector <vector <int> > d(n, vector <int> (n));
 	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < n; j++) {
 			cin >> d[i][j];
+            if (d[i][j] == 100000) {
+                d[i][j] = INF;
+            }
+        }
 
 	for (int k = 0; k < n; k++)
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < n; j++)
-				if (d[i][k] <= 10000 && d[k][j] <= 10000)
-					d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+				if (d[i][k] != INF && d[k][j] != INF)
+					d[i][j] = max(min(d[i][j], d[i][k] + d[k][j]), -1000000000);
 
 	for (int i = 0; i < n; i++)
 		if (d[i][i] < 0)
